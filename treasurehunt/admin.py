8485c27c0ddb5c5ef23f5d67a7fc703db74cdb7e
Abhinav I <abhinav147@rocketmail.com>
2014-10-06 16:22:26 +0530

    Added views for login

--- treasurehunt/admin.py
+++ treasurehunt/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
+from treasurehunt.models import Level, UserProfile
 
-# Register your models here.
+
+admin.site.register(UserProfile)
+admin.site.register(Level)
--- treasurehunt/views.py
+++ treasurehunt/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render
-from django.contrib.auth.models import user
+from django.contrib.auth import authenticate, login, logout
+from django.http import HttpResponseRedirect
 from treasurehunt.models import Level
+from django.core.context_processors import csrf
 
 
 def render_level(request, level):
@@ -10,3 +12,30 @@ def render_level(request, level):
 		level_content = Level.objects.filter(level=level)
 		return render(request, 'treasurehunt/level.html', )
 
+
+def user_login(request):
+	if request.method == 'GET':
+		protected_form = {}
+		form.update(csrf(request))
+
+		return render(request, 'login.html', protected_form)
+
+	elif request.method == 'POST':
+		login_username = request.POST['username']
+		login_password = request.POST['password']
+
+		user = authenticate(username=login_username, password=login_password)
+
+		if user is not None:
+			login(request, user)
+			return HttpResponseRedirect('/login_success/')
+		else:
+			return HttpResponseRedirect('/login_failure/')
+
+
+def user_logout(request):
+	user = request.user
+
+	if user.is_authenticated():
+		logout(request)
+		return HttpResponseRedirect('/login/')
