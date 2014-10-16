335412494465cb94cd30e79fdb7908a3367a33f0
Abhinav I <abhinav147@rocketmail.com>
2014-10-16 23:03:22 +0530

    Added css and improved UI

--- oth/settings.py
+++ oth/settings.py
@@ -81,6 +81,6 @@ MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
 
 TEMPLATE_DIRS = [os.path.join(BASE_DIR, 'templates')]
